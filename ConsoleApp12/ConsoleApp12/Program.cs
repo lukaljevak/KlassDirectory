9175ef1098e0ab6af5ac80a5d5f2083d493b5b6c
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2024-04-09 11:40:00 +0200

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //unos putanje mape
+            Console.WriteLine("Unesi putanju mape");
+            string putanja = Console.ReadLine();
+            //provjera postoji li mapa
+            if (Directory.Exists(putanja))
+            {
+                Console.WriteLine("Mapa postoji!");
+                //ako postoji ispisuje sve datoteke u mapi
+                Console.WriteLine("Ispis datoteka");
+                foreach (string datoteka in Directory.GetFiles(putanja))
+                {
+                    Console.WriteLine("Datoteka");
+                }
+                //putanja podmape
+                string putanjaPodmape = putanja + "\\bla";
+                //provjera postoji li podmapa, ako ne postoji, kreiramo je
+                if (!Directory.Exists(putanjaPodmape))
+                {
+                    Directory.CreateDirectory(putanjaPodmape);
+                    Console.WriteLine("Mapa" + putanjaPodmape + " je napravljena.");
+                }
+                //ispis podmapa
+                foreach(string podmapa in Directory.GetDirectories(putanja)) 
+                {
+                    Console.WriteLine(podmapa);
+                }
+                Directory.Delete(putanjaPodmape);
+                Console.WriteLine("Ispis podmapa nakon brisanja:");
+            }
+            //ako ne postoji, kreiramo mapu
+            else 
+            {
+                Directory.CreateDirectory(putanja);
+                Console.WriteLine("Mapa" + putanja + "je napravljena");
+            }
+            Console.ReadKey();
+        }
+    }
+}
